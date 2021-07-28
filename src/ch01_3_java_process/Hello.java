ac19db9add30a387001dd84a9099f747fef917c5
bticamp <bticamp@DESKTOP-H0IJBRQ>
2021-07-28 10:10:20 +0900

    ch01-2

--- src/ch01_3_java_process/Hello.java
+++ src/ch01_3_java_process/Hello.java
@@ -2,6 +2,34 @@ package ch01_3_java_process;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("Hello Worlddddd!");
+		System.out.println("Hello \nworld~!!");	// 헬로우월드라고 쓴것임 \n은 줄바꿈
+		
+		
+		/************************************
+		 * 
+		 * 여기부터 여기까지
+		 * 주석임
+		 * 
+		 ******************************/
+		
+		
+		/***************************
+		 * 클래스 앞에 클래스 설명이나 입출력 내용등을 적을 수 있다
+		 * 아무튼 많이 쓴다
+		 * arg가 나타내는게 모냐.... arg는 사실 아무것도 아니다 걍 이름이었다 등등
+		 */
+		
+		/**
+		 * 
+		 * @author bticamp
+		 */
+		
+{
+	
+		int x = 1;	//변수 x를 선언하고 1을 저장
+		int y = 2;	//변수 y를 선언하고 2를 저장
+		int result = x + y;	//변수 result를 선언하고 x와 y를 더한 값을 저장
+				System.out.println(result);	//모니터에 출력하는 메소드 호출
+		}
 	}
-}
+} 
